056946f5e9dd300a4b345819d9e0e7625bc92b1f
Dkpe <66471698+Dkpe@users.noreply.github.com>
2020-08-11 12:05:17 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1131,8 +1131,8 @@ unsigned int static GetNextWorkRequired(const CBlockIndex* pindexLast, const CBl
     if (pindexLast == NULL)
         return nProofOfWorkLimit;
     
-    if (pindexLast->nHeight >= 1525)
-        nTargetTimespan = 4 * 60; // 1 BLOCK after block 1525
+    if (pindexLast->nHeight >= 2600)
+        nTargetTimespan = 4 * 60; // 1 BLOCK after block 2600
     int64 nInterval = nTargetTimespan / nTargetSpacing;
 
     // Only change once per interval
