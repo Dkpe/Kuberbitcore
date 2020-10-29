18c06c28b2418bd117e2fbe567d5d5d0ba895ab5
Dkpe <66471698+Dkpe@users.noreply.github.com>
2020-10-29 15:28:09 +0200

    // Copyright update
    (c) 2020 The Kuberbitcoin developers

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) 2010 Satoshi Nakamoto
 // Copyright (c) 2009-2014 The Bitcoin developers
-// Copyright (c) 2013-2014 The Kuberbitcoin developers
-// Copyright (c)      2014 The Inutoshi developers
+// Copyright (c) 2020 The Kuberbitcoin developers
+
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
